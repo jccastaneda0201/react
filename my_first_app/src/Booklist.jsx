e8296f2f35cf15d546a79ed017da2dad5f580336
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-06-10 18:35:05 +0200

    initial commit

--- my_first_app/src/Booklist.jsx
+++ my_first_app/src/Booklist.jsx
@@ -1,29 +1,36 @@
 import './Booklist.css'
 
+const books = [
+  {
+    author: 'Mel Robbins',
+    title: 'The Let Them Theory',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91ZVf3kNrcL._AC_UL600_SR600,400_.jpg',
+    id: 1
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+    id: 2
+  },
+];
 
-const firstBook = {
-  author: 'Mel Robbins',
-  title: 'The Let Them Theory',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/91ZVf3kNrcL._AC_UL600_SR600,400_.jpg',
-};
-const secondBook = {
-  author: 'James Clear',
-  title: 'Atomic Habits',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
-};
 
 export const Booklist = () => {
   return (
     <section className='booklist'>
-      <Book author={firstBook.author} title={firstBook.title} img={firstBook.img} />
-      <Book author={secondBook.author} title={secondBook.title} img={secondBook.img} />
+      {books.map((book) => {
+        return (
+          <Book {...book} key={book.id} />
+        )
+      })}
     </section>
   )
 }
 
-const Book = ({ img, title, author }) => {
+const Book = (props) => {
   //Destructuring
-  // const { img, title, author } = props
+  const { img, title, author } = props
   return (
     <article className='book'>
       <img src={img} alt={title} />
